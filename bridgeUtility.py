06e54c2161c903e9c94f06a9e965e55aeccf6167
Chris <crsayen@gmail.com>
2020-05-20 13:27:07 -0400

    added tkinter interface

--- bridgeUtility.py
+++ bridgeUtility.py
@@ -1,38 +1,66 @@
+try:
+    import os
+    import tkinter as tk
+    from tkinter import messagebox
+    import tkinter.ttk as ttk
+    from tkinter import filedialog
+except ImportError as e:
+    print(e)
+    import Tkinter as tk
+    import ttk
+    import tkFileDialog as filedialog
 from paramiko import SSHClient, AutoAddPolicy
 import sys
-import socket
-import threading
-import sys
-import time
 from paramiko.py3compat import u
-
-# windows does not have termios...
 try:
     import termios
     import tty
-
     has_termios = True
 except ImportError:
     has_termios = False
 
-
 ssh_uname = "ubuntu"
 ssh_pass = 'temppwd'
 ssh_ip = "192.168.7.2" if sys.platform == "win32" else "192.168.6.2"
-print(ssh_ip)
 
-ssh = SSHClient()
-ssh.set_missing_host_key_policy(AutoAddPolicy)
-ssh.load_system_host_keys('./keys')
-ssh.connect(hostname=ssh_ip, username=ssh_uname, password=ssh_pass)
+class App:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("300x100")
+        self.root.title('bridge utility')
+        self.startCarBtn = tk.Button(self.root, text="start with car config", command=self.startCar)
+        self.startCarBtn.pack()
+        self.startToolBtn = tk.Button(self.root, text="start with tool config", command=self.startTool)
+        self.startToolBtn.pack()
+        self.stopBtn = tk.Button(self.root, text="start", command=self.stop)
+        self.stopBtn['state'] = 'disabled'
+        self.stopBtn.pack()
+        self.ssh = SSHClient()
+        self.ssh.set_missing_host_key_policy(AutoAddPolicy)
 
-# ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('/home/ubuntu/.nvm/versions/node/v10.20.1/bin/node dev/node-can-bridge/client.js car')
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('ls')
-for line in ssh_stdout:
-   print(line.strip('\n'))
-for line in ssh_stderr:
-   print(line.strip('\n'))
 
-while True:
-    time.sleep(0.2)
-    pass
+    def startCar(self):
+        self.ssh.connect(hostname=ssh_ip, username=ssh_uname, password=ssh_pass)
+        self.ssh.exec_command('/usr/bin/python3.8 dev/node-can-bridge/enable_can.py')
+        self.ssh.exec_command(f'/home/ubuntu/.nvm/versions/node/v10.20.1/bin/node dev/node-can-bridge/client.js car', get_pty=True)
+        self.stopBtn['state'] = 'enabled'
+        self.startCarBtn['state'] = 'disabled'
+        self.startToolBtn['state'] = 'disabled'
+
+    def startTool(self):
+        self.ssh.connect(hostname=ssh_ip, username=ssh_uname, password=ssh_pass)
+        self.ssh.exec_command('/usr/bin/python3.8 dev/node-can-bridge/enable_can.py')
+        self.ssh.exec_command(f'/home/ubuntu/.nvm/versions/node/v10.20.1/bin/node dev/node-can-bridge/client.js tool', get_pty=True)
+        self.stopBtn['state'] = 'enabled'
+        self.startCarBtn['state'] = 'disabled'
+        self.startToolBtn['state'] = 'disabled'
+    
+    def stop(self):
+        self.ssh.close()
+        self.ssh = SSHClient()
+        self.stopBtn['state'] = 'disabled'
+        self.startCarBtn['state'] = 'enabled'
+        self.startToolBtn['state'] = 'enabled'
+
+if __name__ == "__main__":
+    App().root.mainloop()
